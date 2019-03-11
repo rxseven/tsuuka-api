6aa9a05a90f82ac423ebdc57c29dc9b316d050a4
ME (Mac mini) <rxseven.com@gmail.com>
2019-03-12 00:05:27 +0700

    Specify Fixer’s API URL

--- src/config/server.ts
+++ src/config/server.ts
@@ -4,10 +4,13 @@ const API_VERSION = 1;
 // Port
 const PORT = process.env.PORT || 5000;
 
+// URLs
+const FIXER_API_URL = 'http://data.fixer.io/api/latest';
+
 // Client origin
 const CLIENT_ORIGIN =
   process.env.NODE_ENV === 'production'
     ? 'https://tsuuka.herokuapp.com'
     : `http://localhost:3000`;
 
-export { API_VERSION, CLIENT_ORIGIN, PORT };
+export { API_VERSION, CLIENT_ORIGIN, FIXER_API_URL, PORT };
