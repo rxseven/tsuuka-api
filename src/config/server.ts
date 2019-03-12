65c5ce91d9f7ce3593cc5caac493b3ff16af4983
ME (Mac mini) <rxseven.com@gmail.com>
2019-03-12 14:51:09 +0700

    Add additional client origins for development environment

--- src/config/server.ts
+++ src/config/server.ts
@@ -10,7 +10,16 @@ const RESTCOUNTRIES_API_URL = 'https://restcountries.eu/rest/v2';
 
 // Client origin
 const whitelist = {
-  development: ['http://localhost:3000'],
+  development: [
+    'http://localhost:3000',
+    'http://localhost:6006',
+    'http://localhost:8080',
+    'http://localhost:8081',
+    'http://127.0.0.1:3000',
+    'http://127.0.0.1:6006',
+    'http://127.0.0.1:8080',
+    'http://127.0.0.1:8081'
+  ],
   production: ['https://tsuuka.herokuapp.com']
 };
 
